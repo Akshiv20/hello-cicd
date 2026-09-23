622d08445a0c48d43657d6a0735fdd4c522223b9
root <root@ip-172-31-13-174.us-east-2.compute.internal>
2026-09-23 13:26:55 +0000

    Initial Java CI/CD appliaction

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void testMessage() {
+        assertEquals(
+            "Hello from CI/CD Pipeline!",
+            App.getMessage()
+        );
+    }
+}
+
